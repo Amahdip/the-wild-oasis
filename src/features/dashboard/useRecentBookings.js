336bfff5865640b75987bef51f4e28670d713470
amirmahdi <amirmahdiparsaei@gmail.com>
2023-10-30 16:02:22 +0330

    completed

--- src/features/dashboard/useRecentBookings.js
+++ src/features/dashboard/useRecentBookings.js
@@ -1,6 +1,20 @@
-const useRecentBookings = () => {
+import { useQuery } from "@tanstack/react-query";
+import { subDays } from "date-fns";
+import { useSearchParams } from "react-router-dom";
+import { getBookingsAfterDate } from "../../services/apiBookings";
 
-  const [searchParams] = useSearchParam()
-  const numDays = !searchParams ? 7 : Number(searchParams.get("last"))
+export function useRecentBookings() {
+  const [searchParams] = useSearchParams();
 
+  const numDays = !searchParams.get("last")
+    ? 7
+    : Number(searchParams.get("last"));
+  const queryDate = subDays(new Date(), numDays).toISOString();
+
+  const { isLoading, data: bookings } = useQuery({
+    queryFn: () => getBookingsAfterDate(queryDate),
+    queryKey: ["bookings", `last-${numDays}`],
+  });
+
+  return { isLoading, bookings };
 }
