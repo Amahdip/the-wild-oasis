10608172b4148b85b97d8c9ef8f5c408bc2d8364
amirmahdi <amirmahdiparsaei@gmail.com>
2023-10-30 18:34:44 +0330

    account page fixed

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -7,16 +7,10 @@ function Account() {
   return (
     <>
       <Heading as="h1">Update your account</Heading>
-
       <Row>
         <Heading as="h3">Update user data</Heading>
         <UpdateUserDataForm />
         <UpdatePasswordForm />
-      </Row>
-
-      <Row>
-        <Heading as="h3">Update password</Heading>
-        <p>Update user password form</p>
       </Row>
     </>
   );
